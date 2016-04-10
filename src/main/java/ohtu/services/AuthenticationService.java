49dd1737086c7fc712c4dacb599d648b79fcff27
Artur Lindholm <artur@cs.helsinki.fi>
2016-04-10 15:27:46 +0300

    preparations

---
+++ src/main/java/ohtu/services/AuthenticationService.java
@@ -0,0 +1,60 @@
+package ohtu.services;
+
+import ohtu.domain.User;
+import java.util.ArrayList;
+import java.util.List;
+import ohtu.data_access.UserDao;
+
+public class AuthenticationService {
+
+    private UserDao userDao;
+
+    public AuthenticationService(UserDao userDao) {
+        this.userDao = userDao;
+    }
+
+    public boolean logIn(String username, String password) {
+        for (User user : userDao.listAll()) {
+            if (user.getUsername().equals(username)
+                    && user.getPassword().equals(password)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public boolean createUser(String username, String password) {
+        if (userDao.findByName(username) != null) {
+            return false;
+        }
+
+        if (invalid(username, password)) {
+            return false;
+        }
+
+        userDao.add(new User(username, password));
+
+        return true;
+    }
+
+    private boolean invalid(String username, String password) {
+        if (password.length() > 7 && username.length() > 2) {         
+            char c;  
+            int count = 0;   
+            for (int i = 0; i < password.length(); i++) {  
+                c = password.charAt(i);  
+                if (!Character.isLetterOrDigit(c)) {          
+                    return false;  
+                } else if (Character.isDigit(c)) {  
+                    count++;     
+                }  
+            }  
+            if (count < 1)   {     
+                return true;  
+            }     
+            return false;
+        }  
+        return true;  
+    }  
+}
